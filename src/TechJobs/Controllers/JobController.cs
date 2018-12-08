bba70ad74279c3ab84f69f2d3d2f4d9777b2150d
Ryan <hayes.ryan95@gmail.com>
2018-12-08 17:11:29 -0600

    Just finished #6.  Now when someone submits the form for a new job, that data will be added as a new job object.  Also, it redirects you to that one job's information (job ID in the url).

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -48,6 +48,27 @@ namespace TechJobs.Controllers
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
 
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    //Employer = newJobViewModel.
+                    //Location = newJob.Location.Value
+                    
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+
+                };
+
+
+                jobData.Jobs.Add(newJob);
+                return RedirectToAction("Index", new { id = newJob.ID });
+                    
+            }
+
             return View(newJobViewModel);
         }
     }
